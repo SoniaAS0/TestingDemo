846ff1f16b61eea5f661523448b5933e996b67ca
Sonia Arias <eha80877@eduastur.es>
2024-09-25 11:02:49 +0200

    Creadas propiedades clase customer

--- MyLibrary/Customer.cs
+++ MyLibrary/Customer.cs
@@ -3,6 +3,10 @@ namespace MyLibrary
 {
     public class Customer
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
+        public string FullName => $"FirstName LastName";
+      
     }
 }
